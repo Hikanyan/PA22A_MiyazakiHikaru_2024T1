2aa9b68eb81112252ef6ec6750cfd7ae0c579779
KoiiroHikanyan <hikanyan0131@gmail.com>
2024-05-11 18:33:03 +0900

    [Update] StateMachine

--- Main.cpp
+++ Main.cpp
@@ -1,50 +1,82 @@
-﻿# include <Siv3D.hpp>
+﻿#include <Siv3D.hpp>
 
-/*
-	古き良き書き方での実装
-	・安全性や利便性などは一切考えていない
-*/
+class StateMachine;
+//class TitleState;
+class GameState;
 
 namespace constants {
-
 	namespace ball {
-		/// @brief ボールの速さ
 		constexpr double BALL_SPEED = 480.0;
 	}
 
 	namespace bricks {
-		/// @brief ブロックのサイズ
 		constexpr Size BRICK_SIZE{ 40, 20 };
-
-		/// @brief ブロックの数　縦
 		constexpr int Y_COUNT = 5;
-
-		/// @brief ブロックの数　横
 		constexpr int X_COUNT = 20;
-
-		/// @brief 合計ブロック数
 		constexpr int MAX = Y_COUNT * X_COUNT;
 	}
 
 	namespace paddle {
-		constexpr Size SIZE{ 40, 20 };
+		constexpr Size SIZE{ 60, 10 };
 	}
 }
+
+
+
+class State {
+protected:
+	StateMachine* stateMachine;
+
+public:
+	State(StateMachine* machine) : stateMachine(machine) {}
+	virtual void Enter() = 0;
+	virtual void Update() = 0;
+	virtual void Exit() = 0;
+};
+
+class StateMachine {
+private:
+	State* currentState;
+
+public:
+	StateMachine();
+
+	~StateMachine() {
+		if (currentState) {
+			currentState->Exit();
+			delete currentState;
+		}
+	}
+
+	void ChangeState(State* newState) {
+		if (currentState) {
+			currentState->Exit();
+			delete currentState;
+		}
+		currentState = newState;
+		if (currentState) {
+			currentState->Enter();
+		}
+	}
+
+	void Update() {
+		if (currentState) {
+			currentState->Update();
+		}
+	}
+};
 
 class Ball {
 public:
 	Vec2 velocity;
 	Circle model;
 
-	// ボールの初期化
-	Ball() :
-		velocity(0, -constants::ball::BALL_SPEED),
-		model{ 400,400,10 } {}
-	// ボールの更新
+	Ball() : velocity(0, -constants::ball::BALL_SPEED), model{ 400, 400, 10 } {}
+
 	void Update() {
 		model.moveBy(velocity * Scene::DeltaTime());
 	}
-	// ボールの描画
+
 	void Draw() const {
 		model.draw();
 	}
@@ -54,7 +86,6 @@ class Bricks {
 public:
 	Rect bricksModel[constants::bricks::MAX];
 
-	// ブロックの初期化
 	Bricks() {
 		using namespace constants::bricks;
 		for (int y = 0; y < Y_COUNT; ++y) {
@@ -69,37 +100,24 @@ public:
 		}
 	}
 
-	// ブロックの描画
 	void Draw() {
 		for (int i = 0; i < constants::bricks::MAX; ++i) {
-			bricksModel[i].stretched(-1).draw(HSV{ bricksModel[i].y - 40 });
+			bricksModel[i].stretched(-1).draw(HSV(bricksModel[i].y - 40));
 		}
 	}
 
-	// ブロックとボールの衝突判定
 	void Intersects(Ball& ball) {
 		for (int i = 0; i < constants::bricks::MAX; ++i) {
-			// 参照で保持
 			Rect& refBrick = bricksModel[i];
-
-			// 衝突を検知
-			if (refBrick.intersects(ball.model))
-			{
-				// ブロックの上辺、または底辺と交差
-				if (refBrick.bottom().intersects(ball.model) || refBrick.top().intersects(ball.model))
-				{
+			if (refBrick.intersects(ball.model)) {
+				if (refBrick.bottom().intersects(ball.model) || refBrick.top().intersects(ball.model)) {
 					ball.velocity.y *= -1;
 				}
-				else // ブロックの左辺または右辺と交差
-				{
+				else {
 					ball.velocity.x *= -1;
 				}
-
-				// あたったブロックは画面外に出す
-				refBrick.y -= 600;
-
-				// 同一フレームでは複数のブロック衝突を検知しない
-				break;
+				refBrick.y -= 600;  // Move the brick out of the scene
+				break;  // Prevent multiple collisions in the same frame
 			}
 		}
 	}
@@ -109,21 +127,17 @@ class Paddle {
 public:
 	Rect model;
 
-	Paddle() :
-		model(Arg::center(Cursor::Pos().x, 500), 60, 10)
-	{}
+	Paddle() : model(Arg::center(Cursor::Pos().x, 500), constants::paddle::SIZE) {}
 
 	void Update() {
 		model.x = Cursor::Pos().x - (constants::paddle::SIZE.x / 2);
 	}
 
-	// パドルとボールの衝突判定
 	void Intersects(Ball& ball) {
-		if ((0 < ball.velocity.y) && model.intersects(ball.model))
-		{
+		if ((ball.velocity.y > 0) && model.intersects(ball.model)) {
 			ball.velocity = Vec2{
-			(ball.model.x - model.center().x) * 10,
-			-ball.velocity.y
+				(ball.model.x - model.center().x) * 10,
+				-ball.velocity.y
 			}.setLength(constants::ball::BALL_SPEED);
 		}
 	}
@@ -133,27 +147,50 @@ public:
 	}
 };
 
-void Main()
-{
-	// ゲームオブジェクトの生成
+class TitleState : public State {
+public:
+	TitleState(StateMachine* machine) : State(machine) {}
+	void Enter() override { Print << U"TitleState Enter"; }
+	void Update();
+	void Exit() override { Print << U"TitleState Exit"; }
+};
+
+class GameState : public State {
+public:
 	Ball ball;
 	Bricks bricks;
 	Paddle paddle;
 
-	// ゲームループ
-	while (System::Update())
-	{
-		//更新
+	GameState(StateMachine* machine) : State(machine) {}
+
+	void Enter() override {}
+	void Update() override {
 		ball.Update();
 		paddle.Update();
-
-		//当たり判定
 		bricks.Intersects(ball);
 		paddle.Intersects(ball);
-
-		//描画
 		ball.Draw();
 		bricks.Draw();
 		paddle.Draw();
+	}
+	void Exit() override {}
+};
+
+StateMachine::StateMachine() : currentState(nullptr) {
+	currentState = new TitleState(this);
+	currentState->Enter();
+}
+
+void TitleState::Update() {
+	if (KeyEnter.down()) {
+		stateMachine->ChangeState(new GameState(stateMachine));
+	}
+}
+
+void Main()
+{
+	StateMachine sm;
+	while (System::Update()) {
+		sm.Update();
 	}
 }
