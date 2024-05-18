0c013f841333fe02347e56b02ed81c1d8ebe47f1
KoiiroHikanyan <hikanyan0131@gmail.com>
2024-05-18 17:12:43 +0900

    [Update]
    Lifeが減るようになった

--- Main.cpp
+++ Main.cpp
@@ -4,92 +4,149 @@ class StateMachine;
 //class TitleState;
 class GameState;
 
-namespace constants {
-	namespace ball {
+namespace constants
+{
+	namespace ball
+	{
 		constexpr double BALL_SPEED = 480.0;
 	}
 
-	namespace bricks {
-		constexpr Size BRICK_SIZE{ 40, 20 };
+	namespace bricks
+	{
+		constexpr Size BRICK_SIZE{40, 20};
 		constexpr int Y_COUNT = 5;
 		constexpr int X_COUNT = 20;
 		constexpr int MAX = Y_COUNT * X_COUNT;
 	}
 
-	namespace paddle {
-		constexpr Size SIZE{ 60, 10 };
+	namespace paddle
+	{
+		constexpr Size SIZE{60, 10};
+	}
+
+	namespace player
+	{
+		constexpr int MAX_LIFE = 3;
+		constexpr int MAX_SCORE = 0;
 	}
 }
 
 
-
-class State {
+class State
+{
 protected:
 	StateMachine* stateMachine;
 
 public:
-	State(StateMachine* machine) : stateMachine(machine) {}
+	State(StateMachine* machine) : stateMachine(machine)
+	{
+	}
+
 	virtual void Enter() = 0;
 	virtual void Update() = 0;
 	virtual void Exit() = 0;
 };
 
-class StateMachine {
+class StateMachine
+{
 private:
 	State* currentState;
 
 public:
 	StateMachine();
 
-	~StateMachine() {
-		if (currentState) {
+	~StateMachine()
+	{
+		if (currentState)
+		{
 			currentState->Exit();
 			delete currentState;
 		}
 	}
 
-	void ChangeState(State* newState) {
-		if (currentState) {
+	void ChangeState(State* newState)
+	{
+		if (currentState)
+		{
 			currentState->Exit();
 			delete currentState;
 		}
 		currentState = newState;
-		if (currentState) {
+		if (currentState)
+		{
 			currentState->Enter();
 		}
 	}
 
-	void Update() {
-		if (currentState) {
+	void Update()
+	{
+		if (currentState)
+		{
 			currentState->Update();
 		}
 	}
 };
 
-class Ball {
+class Player
+{
+public:
+	int life;
+	int score;
+	bool isAlive;
+
+	Player() : life(constants::player::MAX_LIFE), score(0), isAlive(true)
+	{
+	}
+
+	void DecreaseLife()
+	{
+		if (life > 0)
+		{
+			--life;
+		}
+		else
+		{
+			isAlive = false;
+		}
+	}
+
+	void Update() const
+	{
+		Print << U"Life: " << life;
+		Print << U"Score: " << score;
+	}
+};
+
+class Ball
+{
 public:
 	Vec2 velocity;
 	Circle model;
 
-	Ball() : velocity(0, -constants::ball::BALL_SPEED), model{ 400, 400, 10 } {}
-
-	void Update() {
-		model.moveBy(velocity * Scene::DeltaTime());
+	Ball() : velocity(0, -constants::ball::BALL_SPEED), model{400, 400, 10}
+	{
 	}
 
-	void Draw() const {
+	void Update(Player& player);
+
+	void Draw() const
+	{
 		model.draw();
 	}
 };
 
-class Bricks {
+class Bricks
+{
 public:
 	Rect bricksModel[constants::bricks::MAX];
 
-	Bricks() {
+	Bricks()
+	{
 		using namespace constants::bricks;
-		for (int y = 0; y < Y_COUNT; ++y) {
-			for (int x = 0; x < X_COUNT; ++x) {
+		for (int y = 0; y < Y_COUNT; ++y)
+		{
+			for (int x = 0; x < X_COUNT; ++x)
+			{
 				int index = y * X_COUNT + x;
 				bricksModel[index] = Rect{
 					x * BRICK_SIZE.x,
@@ -100,41 +157,54 @@ public:
 		}
 	}
 
-	void Draw() {
-		for (int i = 0; i < constants::bricks::MAX; ++i) {
+	void Draw()
+	{
+		for (int i = 0; i < constants::bricks::MAX; ++i)
+		{
 			bricksModel[i].stretched(-1).draw(HSV(bricksModel[i].y - 40));
 		}
 	}
 
-	void Intersects(Ball& ball) {
-		for (int i = 0; i < constants::bricks::MAX; ++i) {
+	void Intersects(Ball& ball)
+	{
+		for (int i = 0; i < constants::bricks::MAX; ++i)
+		{
 			Rect& refBrick = bricksModel[i];
-			if (refBrick.intersects(ball.model)) {
-				if (refBrick.bottom().intersects(ball.model) || refBrick.top().intersects(ball.model)) {
+			if (refBrick.intersects(ball.model))
+			{
+				if (refBrick.bottom().intersects(ball.model) || refBrick.top().intersects(ball.model))
+				{
 					ball.velocity.y *= -1;
 				}
-				else {
+				else
+				{
 					ball.velocity.x *= -1;
 				}
-				refBrick.y -= 600;  // Move the brick out of the scene
-				break;  // Prevent multiple collisions in the same frame
+				refBrick.y -= 600;
+				break;
 			}
 		}
 	}
 };
 
-class Paddle {
+class Paddle
+{
 public:
 	Rect model;
 
-	Paddle() : model(Arg::center(Cursor::Pos().x, 500), constants::paddle::SIZE) {}
+	Paddle() : model(Arg::center(Cursor::Pos().x, 500), constants::paddle::SIZE)
+	{
+	}
 
-	void Update() {
+	void Update()
+	{
 		model.x = Cursor::Pos().x - (constants::paddle::SIZE.x / 2);
 	}
 
-	void Intersects(Ball& ball) {
-		if ((ball.velocity.y > 0) && model.intersects(ball.model)) {
+	void Intersects(Ball& ball)
+	{
+		if ((ball.velocity.y > 0) && model.intersects(ball.model))
+		{
 			ball.velocity = Vec2{
 				(ball.model.x - model.center().x) * 10,
 				-ball.velocity.y
@@ -142,49 +212,93 @@ public:
 		}
 	}
 
-	void Draw() const {
+	void Draw() const
+	{
 		model.rounded(3).draw();
 	}
 };
 
-class TitleState : public State {
+class TitleState : public State
+{
 public:
-	TitleState(StateMachine* machine) : State(machine) {}
+	TitleState(StateMachine* machine) : State(machine)
+	{
+	}
+
 	void Enter() override { Print << U"TitleState Enter"; }
 	void Update();
 	void Exit() override { Print << U"TitleState Exit"; }
 };
 
-class GameState : public State {
+class GameState : public State
+{
 public:
+	Player player;
 	Ball ball;
 	Bricks bricks;
 	Paddle paddle;
 
-	GameState(StateMachine* machine) : State(machine) {}
+	GameState(StateMachine* machine) : State(machine)
+	{
+	}
 
-	void Enter() override {}
+	void Enter() override
+	{
+	}
+
 	void Update();
-	void Exit() override {}
+
+	void Exit() override
+	{
+	}
 };
 
-StateMachine::StateMachine() : currentState(nullptr) {
-	currentState = new TitleState(this);
+
+StateMachine::StateMachine() : currentState(new TitleState(this))
+{
 	currentState->Enter();
 }
 
-void TitleState::Update() {
-	if (KeyEnter.down()) {
+void Ball::Update(Player& player)
+{
+	model.moveBy(velocity * Scene::DeltaTime());
+	if (model.x < 0 || model.x > 800)
+	{
+		velocity.x *= -1;
+	}
+	if (model.y < 0)
+	{
+		velocity.y *= -1;
+	}
+	if (model.y > 600)
+	{
+		model.setPos(400, 400);
+		velocity = Vec2(0, -constants::ball::BALL_SPEED);
+		player.DecreaseLife();
+	}
+}
+
+void TitleState::Update()
+{
+	if (KeyEnter.down())
+	{
 		stateMachine->ChangeState(new GameState(stateMachine));
 	}
 }
 
-void GameState::Update() {
-	if (KeyEnter.down()) {
+void GameState::Update()
+{
+	if (KeyEnter.down())
+	{
+		stateMachine->ChangeState(new TitleState(stateMachine));
+	}
+	if(player.isAlive == false)
+	{
 		stateMachine->ChangeState(new TitleState(stateMachine));
 	}
 
-	ball.Update();
+	player.Update();
+	ball.Update(player);
 	paddle.Update();
 	bricks.Intersects(ball);
 	paddle.Intersects(ball);
@@ -192,11 +306,12 @@ void GameState::Update() {
 	bricks.Draw();
 	paddle.Draw();
 }
-
+void 
 void Main()
 {
 	StateMachine sm;
-	while (System::Update()) {
+	while (System::Update())
+	{
 		sm.Update();
 	}
 }
